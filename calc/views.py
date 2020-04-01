57e1cf6f03fac42f5a88f151815a909cb8448116
tlarbgus89 <tlarbgus89@gmail.com>
2020-04-01 23:40:25 +0900

    loaded travello template

--- calc/views.py
+++ calc/views.py
@@ -6,7 +6,7 @@ def home(request):
     return render(request, 'home.html')
 
 def add(request):
-    val1 = request.GET['num1']
-    val2 = request.GET['num2']
-    res = val1 + val2
+    val1 = request.POST['num1']
+    val2 = request.POST['num2']
+    res = int(val1) + int(val2)
     return render(request, 'result.html', {'result':res})
--- trial/urls.py
+++ trial/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('calc.urls')),
+    # path('', include('calc.urls')),
+    path('', include('travello.urls')),
 
 ]
